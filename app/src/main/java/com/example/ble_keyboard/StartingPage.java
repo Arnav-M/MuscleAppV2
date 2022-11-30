d27cee0f12f9e269e85061f51590a80a6ce5a0e6
Arnav-M <75835285+Arnav-M@users.noreply.github.com>
2022-11-29 18:06:31 -0800

    New App version

---
+++ app/src/main/java/com/example/ble_keyboard/StartingPage.java
@@ -0,0 +1,27 @@
+package com.example.ble_keyboard;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting_page);
+        Button btn = (Button)findViewById(R.id.advance);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartingPage.this, MainActivity.class));
+            }
+        });
+    }
+}
