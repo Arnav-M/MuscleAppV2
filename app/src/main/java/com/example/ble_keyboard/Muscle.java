5b6a38a7f02b5dcbdfedb633a703147c5cdd96bd
Arnav-M <75835285+Arnav-M@users.noreply.github.com>
2022-12-01 18:04:27 -0800

    New App version

--- app/src/main/java/com/example/ble_keyboard/Muscle.java
+++ app/src/main/java/com/example/ble_keyboard/Muscle.java
@@ -3,7 +3,8 @@ package com.example.ble_keyboard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.icu.number.LocalizedNumberRangeFormatter;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,9 +30,45 @@ public class Muscle extends AppCompatActivity {
         TextView triceps = findViewById(R.id.Tricep);
         TextView forearm = findViewById(R.id.Forearm);
         for(int i = 0; i < values.length - 4; i+=3) {
-            bicep.setText(values[i]);
-            triceps.setText(values[i+1]);
-            forearm.setText(values[i+2]);
+            int bpercentage = percentage(values[i], values[i + 1], values[i + 2]);
+            int tpercentage = percentage(values[i + 1], values[i], values[i + 2]);
+            int fpercentage = percentage(values[i + 2], values[i], values[i + 1]);
+            bicep.setText(bpercentage);
+            bicep.setTextColor(perc2color(bpercentage));
+            triceps.setText(tpercentage);
+            triceps.setTextColor(perc2color(tpercentage));
+            forearm.setText(fpercentage);
+            forearm.setTextColor(perc2color(fpercentage));
         }
     }
+
+    private int percentage(int value1, int value2, int value3) {
+        int percentage = (value1/(value1 + value2 + value3)) * 100;
+        return percentage;
+    }
+
+    private int perc2color(int percentage) {
+        if(percentage > 80) {
+            // red
+            return Color.parseColor("#FFCC0000");
+        }
+        else if(percentage < 40 && percentage > 20) {
+            // light green
+            return Color.parseColor("#FF99CC00");
+        }
+        else if(percentage < 60 && percentage > 40) {
+            // light orange
+            return Color.parseColor("#FFFFBB33");
+        }
+        else if(percentage < 80 && percentage > 60) {
+            // dark orange
+            return Color.parseColor("#FFFF8800");
+        }
+        else {
+            // dark green
+            return Color.parseColor("#FF669900");
+        }
+    }
+    // Green, light Green, yellow, orange, red
+    // 0-20%, 20%- 40%, 40-60%, 60-80%, 80%-100%
 }
